3f08574ec9ec5aebe2d505dfc06a78820154a1d6
DaviSantanadosSantos21 <davir4771@gmail.com>
2026-07-25 12:11:44 -0300

    fix(src/app): corrige endereço da origem local no CORS

--- src/app.js
+++ src/app.js
@@ -21,7 +21,7 @@ const mensagemRoutes = require('./routes/mensagemRoutes');
 const app = express();
 
 app.use(cors({
-    origin: ['http://127.0.0.1:5500/front-end/index.html',
+    origin: ['http://127.0.0.1:5500',
         'https://smart-global-education-web.vercel.app'
     ],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
